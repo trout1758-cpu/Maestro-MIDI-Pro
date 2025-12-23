43ed141529fac93540215c8260daac6b4e3d34e1
trout1758-cpu <trout1758@gmail.com>
2025-12-23 00:22:34 -0700

    Update export-manager.js
    Added export of ties

--- js/core/export-manager.js
+++ js/core/export-manager.js
@@ -18,23 +18,15 @@ export const ExportManager = {
         State.parts.forEach((part, index) => {
             xml += `  <part id="P${index + 1}">\n`;
             
-            // --- STEP 1: PREPARE AND SORT DATA ---
-            // 1. Sort systems by vertical position
             const systems = part.calibration.map((sys, idx) => ({ ...sys, originalId: idx }));
             systems.sort((a, b) => a.topY - b.topY);
-            
             const systemOrderMap = {};
-            systems.forEach((sys, orderIdx) => {
-                systemOrderMap[sys.originalId] = orderIdx;
-            });
+            systems.forEach((sys, orderIdx) => { systemOrderMap[sys.originalId] = orderIdx; });
 
-            // 2. Sort notes
             const sortedNotes = part.notes.sort((a, b) => {
                 const sysA = systemOrderMap[a.systemId];
                 const sysB = systemOrderMap[b.systemId];
-                
                 if (sysA !== sysB) return sysA - sysB; 
-                
                 if (Math.abs(a.x - b.x) < 5.0) {
                     const getPriority = (type) => {
                         if (type === 'barline') return 0;
@@ -50,32 +42,17 @@ export const ExportManager = {
                 return a.x - b.x;
             });
 
-            // --- STEP 2: SCAN FOR INITIAL ATTRIBUTES ---
-            // Defaults
             let currentClefType = part.clef; 
             let currentBeats = 4;
             let currentBeatType = 4;
             let currentFifths = 0;
 
-            // Look ahead at the beginning of the stream to set initial measure state
-            // Stop scanning when we hit a note or barline
             for (let i = 0; i < sortedNotes.length; i++) {
                 const n = sortedNotes[i];
                 if (n.type === 'note' || n.type === 'rest' || n.type === 'barline') break;
-                
-                if (n.type === 'time') {
-                    const [b, bt] = n.subtype.split('/');
-                    currentBeats = parseInt(b);
-                    currentBeatType = parseInt(bt);
-                }
+                if (n.type === 'time') { const [b, bt] = n.subtype.split('/'); currentBeats = parseInt(b); currentBeatType = parseInt(bt); }
                 if (n.type === 'key') {
-                    // (Reuse keyMap logic for pre-scan if needed, or just let loop handle it)
-                    // For simplicity, we mostly care about Time Sig for the Measure 1 header
-                    const keyMap = {
-                        'C': 0, 'A': 3, 'B': 5, 'D': 2, 'E': 4, 'F': -1, 'G': 1,
-                        'C#': 7, 'F#': 6, 'G#': 8, 'D#': 9, 'A#': 10, 'E#': 11, 'B#': 12,
-                        'Cb': -7, 'Gb': -6, 'Db': -5, 'Ab': -4, 'Eb': -3, 'Bb': -2, 'Fb': -8
-                    };
+                    const keyMap = { 'C': 0, 'A': 3, 'B': 5, 'D': 2, 'E': 4, 'F': -1, 'G': 1, 'C#': 7, 'F#': 6, 'G#': 8, 'D#': 9, 'A#': 10, 'E#': 11, 'B#': 12, 'Cb': -7, 'Gb': -6, 'Db': -5, 'Ab': -4, 'Eb': -3, 'Bb': -2, 'Fb': -8 };
                     if (keyMap.hasOwnProperty(n.subtype)) currentFifths = keyMap[n.subtype];
                 }
                 if (n.type === 'clef') {
@@ -86,89 +63,19 @@ export const ExportManager = {
             }
 
             let measureNum = 1;
-
-            // Start First Measure with PRE-SCANNED state
             xml += `    <measure number="${measureNum}">\n      <attributes>\n        <divisions>24</divisions>\n        <key><fifths>${currentFifths}</fifths></key>\n        <time><beats>${currentBeats}</beats><beat-type>${currentBeatType}</beat-type></time>\n        <clef>\n          <sign>${currentClefType === 'treble' ? 'G' : (currentClefType === 'bass' ? 'F' : 'C')}</sign>\n          <line>${currentClefType === 'treble' ? '2' : (currentClefType === 'bass' ? '4' : '3')}</line>\n        </clef>\n      </attributes>\n`;
             
-            // --- STEP 3: MAIN PROCESSING LOOP ---
             for (let i = 0; i < sortedNotes.length; i++) {
                 const note = sortedNotes[i];
                 const prevNote = i > 0 ? sortedNotes[i-1] : null;
                 const nextNote = i < sortedNotes.length - 1 ? sortedNotes[i+1] : null;
 
-                // --- TIME SIGNATURE EXPORT ---
-                if (note.type === 'time') {
-                    const [beats, beatType] = note.subtype.split('/');
-                    const newBeats = parseInt(beats);
-                    const newBeatType = parseInt(beatType);
-                    
-                    // Only write if it's actually a change from what we just wrote in the header?
-                    // Or strictly write it. MusicXML allows redundant attributes.
-                    // To avoid "double" attributes at start of Measure 1, we can check:
-                    // If we are in Measure 1 and haven't written any notes yet, we might skip redundancy.
-                    // But simpler is to just write it. It updates 'current' for *future* measures.
-                    currentBeats = newBeats;
-                    currentBeatType = newBeatType;
-                    
-                    xml += `      <attributes><time><beats>${currentBeats}</beats><beat-type>${currentBeatType}</beat-type></time></attributes>\n`;
-                    continue;
-                }
+                if (note.type === 'time') { const [beats, beatType] = note.subtype.split('/'); currentBeats = parseInt(beats); currentBeatType = parseInt(beatType); xml += `      <attributes><time><beats>${currentBeats}</beats><beat-type>${currentBeatType}</beat-type></time></attributes>\n`; continue; }
+                if (note.type === 'key') { const keyMap = { 'C': 0, 'A': 3, 'B': 5, 'D': 2, 'E': 4, 'F': -1, 'G': 1, 'C#': 7, 'F#': 6, 'G#': 8, 'D#': 9, 'A#': 10, 'E#': 11, 'B#': 12, 'Cb': -7, 'Gb': -6, 'Db': -5, 'Ab': -4, 'Eb': -3, 'Bb': -2, 'Fb': -8 }; let newFifths = 0; if (keyMap.hasOwnProperty(note.subtype)) newFifths = keyMap[note.subtype]; currentFifths = newFifths; xml += `      <attributes><key><fifths>${currentFifths}</fifths></key></attributes>\n`; continue; }
+                if (note.type === 'symbol') { if (note.subtype === 'segno') xml += `      <direction placement="above"><direction-type><segno/></direction-type></direction>\n`; else if (note.subtype === 'coda') xml += `      <direction placement="above"><direction-type><coda/></direction-type></direction>\n`; continue; }
+                if (note.type === 'clef') { let sign = 'G'; let line = '2'; if (note.subtype === 'treble') { sign = 'G'; line = '2'; currentClefType = 'treble'; } else if (note.subtype === 'bass') { sign = 'F'; line = '4'; currentClefType = 'bass'; } else if (note.subtype === 'c') { sign = 'C'; line = Math.round(5 - (note.pitchIndex / 2)); currentClefType = 'alto'; } xml += `      <attributes><clef><sign>${sign}</sign><line>${line}</line></clef></attributes>\n`; continue; }
+                if (note.type === 'barline') { let barlineXML = ''; if (note.subtype === 'double') barlineXML = '<barline location="right"><bar-style>light-light</bar-style></barline>'; else if (note.subtype === 'final') barlineXML = '<barline location="right"><bar-style>light-heavy</bar-style></barline>'; else if (note.subtype === 'repeat') barlineXML = '<barline location="right"><bar-style>light-heavy</bar-style><repeat direction="backward"/></barline>'; if (barlineXML) xml += `      ${barlineXML}\n`; xml += `    </measure>\n`; measureNum++; xml += `    <measure number="${measureNum}">\n`; continue; }
 
-                // --- KEY SIGNATURE EXPORT ---
-                if (note.type === 'key') {
-                    const keyMap = {
-                        'C': 0, 'A': 3, 'B': 5, 'D': 2, 'E': 4, 'F': -1, 'G': 1,
-                        'C#': 7, 'F#': 6, 'G#': 8, 'D#': 9, 'A#': 10, 'E#': 11, 'B#': 12,
-                        'Cb': -7, 'Gb': -6, 'Db': -5, 'Ab': -4, 'Eb': -3, 'Bb': -2, 'Fb': -8
-                    };
-                    
-                    let newFifths = 0;
-                    if (keyMap.hasOwnProperty(note.subtype)) {
-                        newFifths = keyMap[note.subtype];
-                    }
-                    currentFifths = newFifths;
-                    
-                    xml += `      <attributes><key><fifths>${currentFifths}</fifths></key></attributes>\n`;
-                    continue;
-                }
-
-                // --- SYMBOL EXPORT ---
-                if (note.type === 'symbol') {
-                    if (note.subtype === 'segno') {
-                        xml += `      <direction placement="above"><direction-type><segno/></direction-type></direction>\n`;
-                    } else if (note.subtype === 'coda') {
-                         xml += `      <direction placement="above"><direction-type><coda/></direction-type></direction>\n`;
-                    }
-                    continue; 
-                }
-
-                // --- CLEF EXPORT ---
-                if (note.type === 'clef') {
-                    let sign = 'G'; let line = '2';
-                    if (note.subtype === 'treble') { sign = 'G'; line = '2'; currentClefType = 'treble'; }
-                    else if (note.subtype === 'bass') { sign = 'F'; line = '4'; currentClefType = 'bass'; }
-                    else if (note.subtype === 'c') { sign = 'C'; line = Math.round(5 - (note.pitchIndex / 2)); currentClefType = 'alto'; }
-                    
-                    xml += `      <attributes><clef><sign>${sign}</sign><line>${line}</line></clef></attributes>\n`;
-                    continue;
-                }
-
-                // --- BARLINE ---
-                if (note.type === 'barline') {
-                    let barlineXML = '';
-                    if (note.subtype === 'double') barlineXML = '<barline location="right"><bar-style>light-light</bar-style></barline>';
-                    else if (note.subtype === 'final') barlineXML = '<barline location="right"><bar-style>light-heavy</bar-style></barline>';
-                    else if (note.subtype === 'repeat') barlineXML = '<barline location="right"><bar-style>light-heavy</bar-style><repeat direction="backward"/></barline>';
-
-                    if (barlineXML) xml += `      ${barlineXML}\n`;
-                    
-                    xml += `    </measure>\n`;
-                    measureNum++;
-                    xml += `    <measure number="${measureNum}">\n`;
-                    continue;
-                }
-
-                // --- NOTES / RESTS ---
                 let isChord = false;
                 let isVoice2 = false;
                 let backupDuration = 0;
@@ -176,20 +83,11 @@ export const ExportManager = {
                 if (prevNote && (prevNote.type === 'note' || prevNote.type === 'rest') && (note.type === 'note' || note.type === 'rest')) {
                     if (Math.abs(note.x - prevNote.x) < 2.0) {
                         const sameDuration = (note.duration === prevNote.duration) && (!!note.isDotted === !!prevNote.isDotted);
-                        if (sameDuration) {
-                            isChord = true;
-                        } else {
-                            isVoice2 = true;
-                            let prevDur = (4 * 24) / prevNote.duration;
-                            if (prevNote.isDotted) prevDur *= 1.5;
-                            backupDuration = prevDur;
-                        }
+                        if (sameDuration) { isChord = true; } else { isVoice2 = true; let prevDur = (4 * 24) / prevNote.duration; if (prevNote.isDotted) prevDur *= 1.5; backupDuration = prevDur; }
                     }
                 }
 
-                if (isVoice2) {
-                    xml += `      <backup><duration>${backupDuration}</duration></backup>\n`;
-                }
+                if (isVoice2) { xml += `      <backup><duration>${backupDuration}</duration></backup>\n`; }
 
                 const refMidi = CONFIG.CLEF_OFFSETS[currentClefType] || CONFIG.CLEF_OFFSETS['treble'];
                 let currentMidi = refMidi;
@@ -207,37 +105,37 @@ export const ExportManager = {
                 let durationXML = (4 * 24) / note.duration;
                 if (note.isDotted) durationXML *= 1.5;
                 
-                const typeName = note.duration === 1 ? 'whole' : 
-                                 note.duration === 2 ? 'half' : 
-                                 note.duration === 4 ? 'quarter' : 
-                                 note.duration === 8 ? 'eighth' : '16th';
+                const typeName = note.duration === 1 ? 'whole' : note.duration === 2 ? 'half' : note.duration === 4 ? 'quarter' : note.duration === 8 ? 'eighth' : '16th';
 
-                // --- BEAMING LOGIC ---
+                // --- TIE LOGIC ---
+                const tieStart = note.hasTie;
+                let tieStop = false;
+                if (note.type === 'note') {
+                    for (let j = i - 1; j >= 0; j--) {
+                        const cand = sortedNotes[j];
+                        if (cand.type === 'note') {
+                            if (cand.pitchIndex === note.pitchIndex) {
+                                if (cand.hasTie) {
+                                    tieStop = true;
+                                }
+                                break; 
+                            }
+                        }
+                    }
+                }
+
                 const isBeamable = (n) => n && n.type === 'note' && (n.duration === 8 || n.duration === 16);
-                
-                let beam1 = null; 
-                let beam2 = null; 
-
+                let beam1 = null; let beam2 = null; 
                 if (note.type === 'note' && !isChord && !isVoice2) { 
-                    
                     if (isBeamable(note)) {
-                        const prevIsBeamable = isBeamable(prevNote) && Math.abs(note.x - prevNote.x) > 2.0; 
-                        const nextIsBeamable = isBeamable(nextNote) && Math.abs(nextNote.x - note.x) > 2.0; 
-
-                        if (!prevIsBeamable && nextIsBeamable) {
-                            beam1 = 'begin';
-                        }
-                        else if (prevIsBeamable && !nextIsBeamable) {
-                            beam1 = 'end';
-                        }
-                        else if (prevIsBeamable && nextIsBeamable) {
-                            beam1 = 'continue';
-                        }
-                        
+                        const prevIsBeamable = isBeamable(prevNote) && prevNote.systemId === note.systemId && Math.abs(note.x - prevNote.x) > 2.0; 
+                        const nextIsBeamable = isBeamable(nextNote) && nextNote.systemId === note.systemId && Math.abs(nextNote.x - note.x) > 2.0; 
+                        if (!prevIsBeamable && nextIsBeamable) { beam1 = 'begin'; }
+                        else if (prevIsBeamable && !nextIsBeamable) { beam1 = 'end'; }
+                        else if (prevIsBeamable && nextIsBeamable) { beam1 = 'continue'; }
                         if (note.duration === 16) {
                             const prevIs16 = prevIsBeamable && prevNote.duration === 16;
                             const nextIs16 = nextIsBeamable && nextNote.duration === 16;
-                            
                             if (!prevIs16 && nextIs16) beam2 = 'begin';
                             else if (prevIs16 && !nextIs16) beam2 = 'end';
                             else if (prevIs16 && nextIs16) beam2 = 'continue';
@@ -246,14 +144,9 @@ export const ExportManager = {
                 }
 
                 xml += `      <note>\n`;
-                
-                if (isChord) {
-                    xml += `        <chord/>\n`;
-                }
-
-                if (note.type === 'rest') {
-                     xml += `        <rest/>\n`;
-                } else {
+                if (isChord) { xml += `        <chord/>\n`; }
+                if (note.type === 'rest') { xml += `        <rest/>\n`; } 
+                else {
                      xml += `        <pitch>\n          <step>${stepName}</step>\n`;
                      if (note.accidental) {
                          let alter = 0;
@@ -264,19 +157,21 @@ export const ExportManager = {
                      }
                      xml += `          <octave>${octave}</octave>\n        </pitch>\n`;
                 }
-                
                 xml += `        <duration>${durationXML}</duration>\n`;
-                
-                if (isVoice2) {
-                    xml += `        <voice>2</voice>\n`;
-                } else {
-                    xml += `        <voice>1</voice>\n`;
-                }
-
+                if (isVoice2) { xml += `        <voice>2</voice>\n`; } else { xml += `        <voice>1</voice>\n`; }
                 xml += `        <type>${typeName}</type>\n`;
-                
                 if (note.isDotted) xml += `        <dot/>\n`;
                 if (note.accidental) xml += `        <accidental>${note.accidental}</accidental>\n`;
+                
+                if (tieStop) xml += `        <tie type="stop"/>\n`;
+                if (tieStart) xml += `        <tie type="start"/>\n`;
+                
+                if (tieStop || tieStart) {
+                    xml += `        <notations>\n`;
+                    if (tieStop) xml += `          <tied type="stop"/>\n`;
+                    if (tieStart) xml += `          <tied type="start"/>\n`;
+                    xml += `        </notations>\n`;
+                }
 
                 if (beam1) xml += `        <beam number="1">${beam1}</beam>\n`;
                 if (beam2) xml += `        <beam number="2">${beam2}</beam>\n`;
